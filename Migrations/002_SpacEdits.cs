acfc0f643f857774e7262cc45d96dfafb752930a
Arran France <arranf@gmail.com>
2017-12-21 10:21:27 +0000

    Rename NTC to Spac

--- Migrations/002_SpacEdits.cs
+++ Migrations/002_SpacEdits.cs
@@ -1,26 +1,9 @@
-// <copyright>
-// Copyright 2013 by the Spark Development Network
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </copyright>
-//
-
 using Rock.Plugin;
 
 namespace cc.newspring.AttendedCheckIn.Migrations
 {
     [MigrationNumber( 2, "1.6.10" )]
-    public class NtcEdits : Migration
+    public class SpacEdits : Migration
     {
         public string PersonSNAttributeGuid = "8B562561-2F59-4F5F-B7DC-92B2BB7BB7CF";
         public string GroupSNAttributeGuid = "9210EC95-7B85-4D11-A82E-0B677B32704E";
